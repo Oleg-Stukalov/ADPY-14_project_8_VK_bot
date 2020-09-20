647356ad68977e5e76ca02739bed5c6ad1ca0860
Oleg Stukalov <oleg.stukalov@mail.ru>
2020-09-20 12:28:50 +0300

    VkBot algorithm changed with ERRORS

--- VKbot.py
+++ VKbot.py
@@ -9,7 +9,8 @@ class VkBot:
         print(f"Создан объект бота для пользователя с ID: {user_id}!")
         self._USER_ID = user_id
         self._USERNAME = self._get_user_name_from_vk_id(user_id)
-        self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА"]
+        #self._COMMANDS = ["ПРИВЕТ", "ПОГОДА", "ВРЕМЯ", "ПОКА"] # погода парсится неверно
+        self._COMMANDS = ["ПРИВЕТ", "ВРЕМЯ", "ПОКА"]
 
     # getting user name
     def _get_user_name_from_vk_id(self, user_id):
@@ -26,27 +27,28 @@ class VkBot:
         b = BeautifulSoup(request.text, "html.parser")
         return self._clean_all_tag_from_str(str(b.select(".page")[0].findAll("h2")[1])).split()[1]
 
-    # getting weather
-    def _get_weather(city: str = "санкт-петербург") -> list:
-        request = requests.get("https://sinoptik.com.ru/погода-" + city)
-        #b = bs4.BeautifulSoup(request.text, "html.parser")
-        b = BeautifulSoup(request.text, "html.parser")
-
-        p3 = b.select('.temperature .p3')
-        weather1 = p3[0].getText()
-        p4 = b.select('.temperature .p4')
-        weather2 = p4[0].getText()
-        p5 = b.select('.temperature .p5')
-        weather3 = p5[0].getText()
-        p6 = b.select('.temperature .p6')
-        weather4 = p6[0].getText()
-        result = ''
-        result = result + ('Утром :' + weather1 + ' ' + weather2) + '\n'
-        result = result + ('Днём :' + weather3 + ' ' + weather4) + '\n'
-        temp = b.select('.rSide .description')
-        weather = temp[0].getText()
-        result = result + weather.strip()
-        return result
+    # # getting weather
+    # def _get_weather(city: str) -> list:
+    #     city = 'санкт-петербург'
+    #     request = requests.get("https://sinoptik.com.ru/погода-" + city)
+    #     #b = bs4.BeautifulSoup(request.text, "html.parser")
+    #     b = BeautifulSoup(request.text, "html.parser")
+    #
+    #     p3 = b.select('.temperature .p3')
+    #     weather1 = p3[0].getText()
+    #     p4 = b.select('.temperature .p4')
+    #     weather2 = p4[0].getText()
+    #     p5 = b.select('.temperature .p5')
+    #     weather3 = p5[0].getText()
+    #     p6 = b.select('.temperature .p6')
+    #     weather4 = p6[0].getText()
+    #     result = ''
+    #     result = result + ('Утром :' + weather1 + ' ' + weather2) + '\n'
+    #     result = result + ('Днём :' + weather3 + ' ' + weather4) + '\n'
+    #     temp = b.select('.rSide .description')
+    #     weather = temp[0].getText()
+    #     result = result + weather.strip()
+    #     return result
 
     # tags clearance
     @staticmethod
@@ -76,16 +78,16 @@ class VkBot:
         if message.upper() == self._COMMANDS[0]:
             return f"Привет-привет, {self._USERNAME}!"
 
-        # weather
-        elif message.upper() == self._COMMANDS[1]:
-            return self._get_weather()
+        # # weather
+        # elif message.upper() == self._COMMANDS[1]:
+        #     return self._get_weather()
 
         # Time
-        elif message.upper() == self._COMMANDS[2]:
+        elif message.upper() == self._COMMANDS[1]:
             return self._get_time()
 
         # Bye
-        elif message.upper() == self._COMMANDS[3]:
+        elif message.upper() == self._COMMANDS[2]:
             return f"Пока-пока, {self._USERNAME}!"
 
         else:
--- starter.py
+++ starter.py
@@ -14,23 +14,42 @@ vk = vk_api.VkApi(token=VK_API_KEY)
 longpoll = VkLongPoll(vk)
 
 bot_1 = VkBot(VK_SOI_ID)
-#1111111
+print(bot_1._get_user_name_from_vk_id(VK_SOI_ID))
+#print(bot_1._get_time())
 
-# def write_msg(user_id, message):
-#     vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
-#
-# print("Server started")
-# # main cycle
-# for event in longpoll.listen():
-#     # if we get new message
-#     if event.type == VkEventType.MESSAGE_NEW:
-#
-#         # if message for bot
-#         if event.to_me:
-#             # message from user
-#             request = event.text
-#
-#             # main bot algo
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+print("Сервис запущен!")
+# main cycle
+for event in longpoll.listen():
+    # if we get new message
+    if event.type == VkEventType.MESSAGE_NEW:
+
+        # if message for bot
+        if event.to_me:
+            # message from user
+            #request = event.text # version #1
+            print(f'Новое сообщение от {event.user_id}', end='')
+            bot = VkBot(event.user_id)
+
+            if event.text[0] == "/":
+                write_msg(event.user_id, commander.do(event.text[1::]))
+            else:
+                write_msg(event.user_id, bot.new_message(event.text))
+
+            print('Text: ', event.text)
+            print("-------------------")
+
+
+
+
+
+
+
+            # main bot algo
 #             if request == "привет":
 #                 write_msg(event.user_id, f"Хай, {event.user_id}")
 #             elif request == "пока":
